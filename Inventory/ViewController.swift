bf9e6707230433fc842fcb2259838a6978df77e7
Grant Maloney <grantmaloney@Grants-MacBook-Pro.local>
2018-11-29 14:46:38 -0600

    Initial Commit

---
+++ Inventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory
+//
+//  Created by Grant Maloney on 11/29/18.
+//  Copyright © 2018 Grant Maloney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
